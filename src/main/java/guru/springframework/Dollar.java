80f69cfbbf0d11d5a3b905e1220ce88b7f9fdd80
David Payne <djpdjp16@gmail.com>
2021-01-08 13:23:47 -0600

    TDD - About to start apple vs oranges

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+public class Dollar extends Money {
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(this.amount * multiplier);
+    }
+
+}
