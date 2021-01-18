bcc1e31058021a856735cd45d1a019ee887cdddb
David Payne <djpdjp16@gmail.com>
2021-01-18 10:42:18 -0600

    Everything refactored into Money object only

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -3,15 +3,48 @@ package guru.springframework;
 public class Money {
 
     protected int amount;
+    protected String currency;
 
-
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Money money = (Money) o;
-
-        return amount == money.amount;
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
     }
 
+    protected String currency() {
+        return currency;
+    }
+
+    public Money times(int multiplier) {
+        return new Money(this.amount * multiplier, this.currency);
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+
+        return  (amount == money.amount) && currency == money.currency;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = amount;
+        result = 31 * result + (currency != null ? currency.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 }
